756246d9eddbd52ed4fbf7d35728a11a8cac6398
Cesar Mauricio Hernadez Cubias <cheyosantos2468@gmail.com>
2024-06-01 14:59:57 -0600

    agregado la lectura de datos de la vista monitorizar

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:gatekeeper/screens/home_screen.dart';
 
 import 'package:gatekeeper/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -19,6 +26,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: LoginScreen(),
+
     );
   }
 }
